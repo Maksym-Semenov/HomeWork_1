fde09fea6da8614ba73cf30c6022a8894082b1d4
Maksym-Semenov <mestcomua@gmail.com>
2022-06-21 13:57:04 +0300

    update

--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework1
 {
@@ -10,6 +6,39 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\t\tHomeWork 1");
+            Console.WriteLine();
+            Console.WriteLine("\tPart 1");
+            Console.WriteLine();
+            Console.Write("Enter lehgth of square:\t");
+            bool success1 = int.TryParse(Console.ReadLine(), out int a);
+            Console.WriteLine($"Area = { a * a }");
+            Console.WriteLine($"Perimeter = { a * 4 }");
+            Console.WriteLine("For next step type Enter");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.WriteLine("\tPart 2");
+            Console.WriteLine();
+            Console.Write("What is your name?\t");
+            string name = Console.ReadLine();
+            Console.WriteLine($"How old are you, { name }?");
+            bool success2 = int.TryParse(Console.ReadLine(), out int age);
+            Console.WriteLine($"{ name }, you are { age } years old!");
+            Console.WriteLine("For next step type Enter");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("\tPart 1");
+            Console.WriteLine();
+            Console.Write("Enter lehgth of square:\t");
+            bool success1 = int.TryParse(Console.ReadLine(), out int a);
+            Console.WriteLine($"Area = {a * a}");
+            Console.WriteLine($"Perimeter = {a * 4}");
+            Console.WriteLine("For next step type Enter");
+            Console.ReadKey();
+            Console.WriteLine();
         }
     }
 }
