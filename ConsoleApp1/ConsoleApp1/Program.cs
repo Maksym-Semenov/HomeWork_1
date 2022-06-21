82a4d56a773b975755d5422446df7ebe0e21c5ff
Maksym-Semenov <mestcomua@gmail.com>
2022-06-21 23:39:21 +0300

    finish

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+
+            Console.WriteLine("Enter the value of the side of the square:");
+
+            string inputTheValueOfTheSide = Console.ReadLine();
+            double valueOfSideOfTheSquare = Convert.ToDouble(inputTheValueOfTheSide);
+
+            if (valueOfSideOfTheSquare < 0)
+            {
+                Console.WriteLine("The area cannot be negative");
+            }
+            else
+            {
+                Console.WriteLine("The perimeter of the square: {0} \t" +
+                    "Square area: {1}", valueOfSideOfTheSquare * 4, valueOfSideOfTheSquare * valueOfSideOfTheSquare);
+            }
+
+            Console.WriteLine();
+
+            // Task 2
+
+            Console.WriteLine("What is your name?");
+
+            string InputNameOfUser = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+
+            string InputAgeOfUser = Console.ReadLine();
+            int AgeOfUser = Convert.ToInt32(InputAgeOfUser);
+
+            Console.WriteLine("Name: {0} \t" +
+                "Age: {1}", InputNameOfUser, AgeOfUser);
+
+            Console.WriteLine();
+
+            // Task 3
+
+            Console.WriteLine("Enter the radius of the circle:");
+
+            string InputRadiusOfTheCircle = Console.ReadLine();
+            double radiusOfTheCircle = Convert.ToDouble(InputRadiusOfTheCircle);
+
+            Console.WriteLine("Length: {0:F2} \t" +
+                "Area: {1:F2} \t" +
+                "Volume: {2:F2}", 2 * radiusOfTheCircle * Math.PI, Math.PI * radiusOfTheCircle * radiusOfTheCircle, (4 / 3) * Math.PI * radiusOfTheCircle * radiusOfTheCircle * radiusOfTheCircle);
+
+            Console.ReadKey();
+        }
+
+    }
+}
--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -6,36 +6,64 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
+            //Part 1
             Console.WriteLine("\t\tHomeWork 1");
             Console.WriteLine();
             Console.WriteLine("\tPart 1");
             Console.WriteLine();
-            Console.Write("Enter lehgth of square:\t");
-            bool success1 = int.TryParse(Console.ReadLine(), out int a);
-            Console.WriteLine($"Area = { a * a }");
-            Console.WriteLine($"Perimeter = { a * 4 }");
-            Console.WriteLine("For next step type Enter");
-            Console.ReadKey();
-            Console.WriteLine();
-
+            int a = -1;
+            while ( a <= 0 )
+            {
+                Console.Write("Enter lehgth of square:\t");
+                bool success1 = int.TryParse(Console.ReadLine(), out a);
+                if ( a <= 0 )
+                {
+                    Console.WriteLine("Value must be positive number!");
+                    continue;
+                }
+                Console.WriteLine($"Area = { a * a }");
+                Console.WriteLine($"Perimeter = { a * 4 }");
+                Console.WriteLine("For next step type Enter");
+                Console.ReadKey();
+                Console.WriteLine();
+            }
+            //Part 2
             Console.WriteLine("\tPart 2");
             Console.WriteLine();
             Console.Write("What is your name?\t");
             string name = Console.ReadLine();
-            Console.Write($"How old are you, { name }?\t");
-            bool success2 = int.TryParse(Console.ReadLine(), out int age);
-            Console.WriteLine($"{ name }, you are { age } years old!");
+            int age = 0;
+            while ( age <= 0 | age >= 160 )
+            {
+                Console.Write($"How old are you, {name}?\t");
+                bool success2 = int.TryParse(Console.ReadLine(), out age);
+                if ( age <= 0 | age >= 160 )
+                {
+                    Console.WriteLine("Your age is wrong!");
+                }
+            }
+            Console.WriteLine($"{name}, you are {age} years old!");
             Console.WriteLine("For next step type Enter");
             Console.ReadKey();
             Console.WriteLine();
-
+            //Part 3
             Console.WriteLine("\tPart 3");
             Console.WriteLine();
-            Console.Write("Enter radius of circle:\t");
-            bool success3 = double.TryParse(Console.ReadLine(), out double r);
+            double r = -1;
+            while ( r <= 0 )
+            {
+                Console.Write("Enter radius of circle:\t");
+                bool success3 = double.TryParse(Console.ReadLine(), out r);
+                if ( r <= 0 )
+                {
+                    Console.WriteLine("Value must be positive number!");
+                    continue;
+                }
+            }
             Console.WriteLine($"Length = { 2 * r * Math.PI }");
             Console.WriteLine($"Area = { r * r * Math.PI }");
             Console.WriteLine($"Volume = { ( 4 / 3 ) / ( 3 * r * Math.PI )}");
+            Console.WriteLine();
             Console.WriteLine("Thank you for your work!");
             Console.ReadKey();
         }
